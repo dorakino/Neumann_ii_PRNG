72ab3a4fc4f1f203098404cecef4f8520da54e58
dorakino <42067569+dorakino@users.noreply.github.com>
2019-03-25 07:42:39 -0700

    Update neumann.cpp
    class declaration is removed, header is included

--- neumann.cpp
+++ neumann.cpp
@@ -1,103 +1,6 @@
 #include <iostream>
+#include "neumann.h"
 using namespace std;
-
-class NeumannSecondGenerator{
-private:
-    int a = 0;
-    int b = 0;
-
-    int bev = 0;
-    int per = 0;
-public:
-    void setNumbers(const int& first,const int& second){
-        a = first;
-        b = second;
-    };
-
-    int generateNumber(int aa, int bb){
-        int myArr[4];
-        int result;
-        int numberOfDigits;
-
-        int var = aa * bb;
-        numberOfDigits = 0;
-        int temp = var;
-        while(temp >= 1){
-            temp /= 10;
-            numberOfDigits++;
-        }
-        int temp1 = numberOfDigits;
-        for(int i = 4; i > 0; i--){
-            if(temp1 > 0){
-                myArr[i-1] = var % 10 ;
-                var /= 10;
-                temp1--;
-            }
-            else{
-                myArr[i-1] = 0;
-            }
-        }
-        if(numberOfDigits < 3){
-            result = myArr[2] * 10 + myArr[3];
-        }
-        else{
-            result = myArr[1] * 10 + myArr[2];
-        }
-        return result;
-    };
-    void method(){
-        int cnt = 2;
-        int tempArr[200];
-        tempArr[0] = a;
-        tempArr[1] = b;
-
-        int myInt1 = a;
-        int myInt2 = b;
-
-        int temp6;
-        int temp7;
-
-        bool appeared = false;
-        while(!appeared){
-            temp6 = generateNumber(myInt1,myInt2);
-            temp7 = generateNumber(myInt2,temp6);
-
-            tempArr[cnt] = temp6;
-            int j = 0;
-            while(j < cnt)
-            {
-                if((tempArr[j] == temp6) && (tempArr[j+1] == temp7)){
-                    appeared = true;
-                    bev = j;
-                    per = cnt - bev;
-                    break;
-                }
-                j++;
-            }
-            if(cnt == 2 && bev == 0){
-                per = 1;
-            }
-            myInt1 = myInt2;
-            myInt2 = temp6;
-            cnt++;
-        }
-    };
-    int getFirst(){
-        return a;
-    };
-
-    int getSecond(){
-        return b;
-    };
-
-    int getBev(){
-        return bev;
-    };
-
-    int getPer(){
-        return per;
-    };
-};
 
 int main()
 {
@@ -117,20 +20,14 @@ int main()
         }
     }
 
-    int myVar1 = 0;
-    int myVar2 = 0;
-
     int total1 = 1;
     int total2 = 1;
 
     for(int i = 0; i < 100; i++){
         for(int j = 0; j < 100; j++){
             int cnt1 = 1;
-            myVar1 = i;
-            myVar2 = j;
-            oneCase.setNumbers(myVar1,myVar2);
+            oneCase.setNumbers(i,j);
             oneCase.method();
-     //       int temp4 = cnt1;
             int h = 0;
             while(h < total1){
                 if(numberOfBevs[h][0] == oneCase.getBev()){
@@ -173,11 +70,11 @@ int main()
         }
     }
 
-    cout << "bevezeto szakaszok:" << endl;
+    cout << "length of initiatory lines, and the number of their appearance:" << endl;
     for(int i = 0; i < total1; i++){
         cout << numberOfBevs[i][0] << "   " << numberOfBevs[i][1] << endl;
     }
-    cout << "periodusok:" << endl;
+    cout << "length of periods, and the number of their appearance:" << endl;
     for(int i = 0 ; i < total2; i++){
         cout << numberOfPers[i][0] << "   " << numberOfPers[i][1] << endl;
     }
