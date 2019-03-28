64320f62882fe7f421dd2c423259023c508c2d48
dorakino <42067569+dorakino@users.noreply.github.com>
2019-03-28 10:51:26 -0700

    Update neumann.cpp
    rewritten calculation

--- neumann.cpp
+++ neumann.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "neumann.h"
+#include "hey.h"
+
 using namespace std;
 
 int main()
@@ -10,12 +11,16 @@ int main()
     vector< vector<int> > vecNumberOfBevs;
     vector< vector<int> > vecNumberOfPers;
 
-    vector<int> temp;
-    temp.push_back(0);
-    temp.push_back(0);
+    vector<int> temp1;
+    temp1.push_back(0);
+    temp1.push_back(0);
 
-    vecNumberOfBevs.push_back(temp);
-    vecNumberOfPers.push_back(temp);
+    vector<int> temp2;
+    temp2.push_back(1);
+    temp2.push_back(0);
+
+    vecNumberOfBevs.push_back(temp1);
+    vecNumberOfPers.push_back(temp2);
 
     int total1 = vecNumberOfBevs.size();
     int total2 = vecNumberOfPers.size();
@@ -35,16 +40,11 @@ int main()
             }
             if(h == total1){
                 vector<int> myrow1;
-                if((vecNumberOfBevs[h-1][0] == 0) && (vecNumberOfBevs[h-1][1] == 0)){
-                    vecNumberOfBevs[h-1][0] = oneCase.getBev();
-                    vecNumberOfBevs[h-1][1] = 1;
-                }
-                else{
+
                     myrow1.push_back(oneCase.getBev());
                     myrow1.push_back(1);
                     vecNumberOfBevs.push_back(myrow1);
                     total1++;
-                }
             }
 
             int l = 0;
@@ -57,16 +57,10 @@ int main()
             }
             if(l == total2){
                 vector<int> myrow2;
-                if((vecNumberOfPers[l-1][0] == 0) && (vecNumberOfPers[l-1][1] == 0)){
-                    vecNumberOfPers[l-1][0] = oneCase.getPer();
-                    vecNumberOfPers[l-1][1] = 1;
-                }
-                else{
                     myrow2.push_back(oneCase.getPer());
                     myrow2.push_back(1);
                     vecNumberOfPers.push_back(myrow2);
                     total2++;
-                }
             }
         }
     }
@@ -83,5 +77,6 @@ int main()
     for(int i = 0 ; i < total2; i++){
         cout << vecNumberOfPers[i][0] << "   " << vecNumberOfPers[i][1] << endl;
     }
+
     return 0;
 }
